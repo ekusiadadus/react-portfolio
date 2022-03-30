35d5ffe64d1b0b0f1c3af35da3c4d2827553aa07
ekusiadadus <ekusiadadus@gmail.com>
2022-03-30 14:00:09 +0000

    add: snsicon

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,7 +1,15 @@
 import './index.scss'
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 import LogoS from '../../assets/images/logo-s.png'
 import LogoSubtitle from '../../assets/images/logo-sub.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHome, faUser, faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import {
+  faGithub,
+  faLinkedin,
+  faTwitter,
+  faFigma,
+} from '@fortawesome/free-brands-svg-icons'
 
 const Sidebar = () => {
   return (
@@ -10,6 +18,70 @@ const Sidebar = () => {
         <img src={LogoS} alt="logo"></img>
         <img className="sub-logo" src={LogoSubtitle} alt="logosubtitle"></img>
       </Link>
+      <nav>
+        <NavLink exact="true" activeclassname="active" to="/">
+          <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+        </NavLink>
+
+        <NavLink
+          exact="true"
+          activeclassname="active"
+          className="about-link"
+          to="/about"
+        >
+          <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+        </NavLink>
+
+        <NavLink
+          exact="true"
+          activeclassname="active"
+          className="contact-link"
+          to="/contact"
+        >
+          <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+        </NavLink>
+      </nav>
+      <ul>
+        <li>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://www.linkedin.com/in/ekusiadadus"
+          >
+            <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+          </a>
+        </li>
+
+        <li>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://www.twitter.com/ekusiadadus"
+          >
+            <FontAwesomeIcon icon={faTwitter} color="#4d4d4e" />
+          </a>
+        </li>
+
+        <li>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://www.github.com/ekusiadadus"
+          >
+            <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+          </a>
+        </li>
+
+        <li>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://zenn.dev/ekusiadadus"
+          >
+            <FontAwesomeIcon icon={faFigma} color="#4d4d4e" />
+          </a>
+        </li>
+      </ul>
     </div>
   )
 }
